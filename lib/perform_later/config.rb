26ee3f022f804164e069cecd4359b363e5a8fc52
Avi Tzurel <avi@kensodev.com>
2012-11-14 18:03:09 +0200

    Loner prefix is now configurable

--- lib/perform_later/config.rb
+++ lib/perform_later/config.rb
@@ -1,5 +1,11 @@
 module PerformLater
   class Config
+    def self.loner_prefix=(prefix)
+      @prefix = prefix
+    end
+    def self.loner_prefix
+      @prefix || "loner"
+    end
     def self.enabled=(value)
       @enabled = value
     end
--- lib/perform_later/payload_helper.rb
+++ lib/perform_later/payload_helper.rb
@@ -3,9 +3,13 @@ module PerformLater
     def self.get_digest(klass, method, *args)
       args = args.flatten
       payload = {:class => klass, :method => method.to_s, :args => args}.to_s
-      digest = Digest::MD5.hexdigest("#{payload}")
-      digest = "loner:#{digest}"
-      digest
+
+      digest = []
+
+      digest << PerformLater.config.loner_prefix
+      digest << Digest::MD5.hexdigest("#{payload}")
+
+      digest.join(":")
     end
   end
 end
